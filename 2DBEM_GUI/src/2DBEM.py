78673c239678e848319525c68ed17eb41da327f1
KWGHG <k41602@gmail.com>
2023-03-10 02:14:27 +0800

    0310

--- 2DBEM_GUI/src/2DBEM.py
+++ 2DBEM_GUI/src/2DBEM.py
@@ -14,13 +14,12 @@ from Subui import Ui_subWindow
 
 class myMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        
-        super(myMainWindow ,self).__init__() 
-        icon = self.resource_path(os.path.join("src" ,"plane1.png"))
-        #print(icon)
+        super(myMainWindow, self).__init__()
+        icon = self.resource_path(os.path.join("src", "plane1.png"))
+        # print(icon)
         self.setWindowIcon(QtGui.QIcon(icon))
         self.ui = Ui_mainWindow()
-        
+
         self.ui.setupUi(self)
         self.velocity = 0
         self.angle_of_attack = 0
@@ -31,28 +30,30 @@ class myMainWindow(QtWidgets.QMainWindow):
         self.filePath = ""
         self.init()
         self.msg()
-    def resource_path(self ,relative_path):
-        if hasattr(sys, '_MEIPASS'):
+
+    def resource_path(self, relative_path):
+        if hasattr(sys, "_MEIPASS"):
             base_path = sys._MEIPASS
-            #print("YES " ,base_path)
+            # print("YES " ,base_path)
         else:
             base_path = os.path.abspath("")
-            #print("NO" ,base_path)
-        return  os.path.join(base_path ,relative_path)
+            # print("NO" ,base_path)
+        return os.path.join(base_path, relative_path)
 
-    def init(self): 
+    def init(self):
         self.ui.Message_Label.setText(" Message ")
         self.ui.Angle_Input.setText("0")
         self.ui.Panels_Input.setText("0")
         self.ui.Velocity_Input.setText("0")
         self.setup_control()
-    
+
     def msg(self):
         mbox = QtWidgets.QMessageBox()
         mbox.setIcon(2)
         mbox.setText("Airfoil data must be in clockwise from trailing edge")
         mbox.setWindowTitle("Hint")
         mbox.exec_()
+
     def setup_control(self):
         self.ui.Load.clicked.connect(self.file_load)
         self.ui.PLOT.clicked.connect(self.airfoil_plot)
@@ -63,8 +64,6 @@ class myMainWindow(QtWidgets.QMainWindow):
         self.ui.CP_Contour_Plot.clicked.connect(self.CP_plot)
         self.ui.Download.clicked.connect(self.download_Cp)
 
-
-
     def file_load(self):
         self.filePath, filterType = QtWidgets.QFileDialog.getOpenFileName(
             self, "Open file", "./"
@@ -74,7 +73,9 @@ class myMainWindow(QtWidgets.QMainWindow):
         if self.filePath == "" or self.numbers_of_panel == 0:
             self.ui.Message_Label.setText("Input numbers of panel or loading file ")
         else:
-            self.geometry = np.array(parametric_airfoil(self.filePath, self.numbers_of_panel))
+            self.geometry = np.array(
+                parametric_airfoil(self.filePath, self.numbers_of_panel)
+            )
             plt.cla()
             fig = plt.figure(figsize=(5, 2))
             ax = fig.add_axes([0.15, 0.25, 0.7, 0.7])
@@ -89,7 +90,6 @@ class myMainWindow(QtWidgets.QMainWindow):
             self.ui.graphicsView.setScene(graphicscene)
             self.ui.graphicsView.show()
 
-
     def velocity_onButtonClick(self):
         velocity = float(self.ui.Velocity_Input.text())
         self.velocity = velocity
@@ -99,17 +99,18 @@ class myMainWindow(QtWidgets.QMainWindow):
         self.angle_of_attack = angle_of_attack
 
     def numbers_of_panel_onButtonClick(self):
-        #if self.filePath == "" or self.numbers_of_panel == 0:
+        # if self.filePath == "" or self.numbers_of_panel == 0:
         #    self.ui.Message_Label.setText("Input numbers of panel ")
-        #else:
+        # else:
         self.numbers_of_panel = float(self.ui.Panels_Input.text())
         if self.filePath != "":
-            self.geometry = np.array(parametric_airfoil(self.filePath, self.numbers_of_panel))
-            
+            self.geometry = np.array(
+                parametric_airfoil(self.filePath, self.numbers_of_panel)
+            )
 
     def calculation(self):
-        if self.geometry.size == 0 or self.velocity == 0 :
-            self.ui.Message_Label.setText(" Please input file ")
+        if self.geometry.size == 0 or self.velocity == 0:
+            self.ui.Message_Label.setText(" Please input file or input parameters ")
         else:
             self.CL, self.CP = BEM(self.velocity, self.angle_of_attack, self.geometry)
             self.ui.CL_Label.setText(str(round(self.CL, 3)))
@@ -145,7 +146,7 @@ class myMainWindow(QtWidgets.QMainWindow):
 
 
 if __name__ == "__main__":
-    #print("Work dir:" + os.getcwd())
+    # print("Work dir:" + os.getcwd())
     app = QtWidgets.QApplication(sys.argv)
     window = myMainWindow()
     window.show()
--- 2DBEM_GUI/src/parameters.py
+++ 2DBEM_GUI/src/parameters.py
@@ -2,6 +2,7 @@ from scipy import interpolate
 import math
 import numpy as np
 
+
 def parametric_airfoil(filename, numbers_of_panel):
     row = 0
     file = open(filename)
@@ -18,75 +19,53 @@ def parametric_airfoil(filename, numbers_of_panel):
     for line in lines:
         list1 = line.strip().split()
         data[row, :] = list1[:]
-        if data[row,0] == 0:
+        if data[row, 0] == 0:
             upperrow = row
         row += 1
     file.close()
+    print("upperrow : ", upperrow)
     lowersurfaceX = np.flip(data[0 : int(upperrow + 1), 0])
     lowersurfaceY = np.flip(data[0 : int(upperrow + 1), 1])
     uppersurfaceX = data[int(upperrow) : int(row), 0]
     uppersurfaceY = data[int(upperrow) : int(row), 1]
-    #print('lowersurfaceX: ',lowersurfaceX)
-    #print('uppersurfaceX: ',uppersurfaceX)
-    lowersurface_interpolate_function = interpolate.CubicSpline(
-        lowersurfaceX, lowersurfaceY
+    lowersurface_interpolate_function = interpolate.interp1d(
+        lowersurfaceX, lowersurfaceY, kind="linear"
     )
-    uppersurface_interpolate_function = interpolate.CubicSpline(
-        uppersurfaceX, uppersurfaceY
+    uppersurface_interpolate_function = interpolate.interp1d(
+        uppersurfaceX, uppersurfaceY, kind="linear"
     )
 
-    lowersurface_panel = int(numbers_of_panel / 2 )
-    uppersurface_panel = int(numbers_of_panel / 2 )
+    lowersurface_panel = int(numbers_of_panel / 2)
+    uppersurface_panel = int(numbers_of_panel / 2)
 
-    
-
-    #Temp = np.append(
+    # Temp = np.append(
     #    np.linspace(1 ,0.9 ,int(lowersurface_panel * 0.3)),
     #    np.linspace(0.8999999 ,0.02 ,int(lowersurface_panel * 0.3)),
-    #)
+    # )
 
-    #new_lowersurfaceX = np.append(
+    # new_lowersurfaceX = np.append(
     #    Temp,
     #    #np.logspace(1 ,10 ,int(lowersurface_panel * 0.4) ,base=0.2),
     #    np.linspace(0.019999,0,10)
-    #)
-    Temp = np.linspace(0 ,math.pi ,lowersurface_panel)
-    new_lowersurfaceX = np.zeros(lowersurface_panel)
-    for i in range(lowersurface_panel):
-        new_lowersurfaceX[i] = 0.5 + 0.5 * math.cos(Temp[i] )
-        #print(new_lowersurfaceX[i])
-    
+    # )
+    Temp = np.linspace(0, math.pi, int(lowersurface_panel))
+    new_lowersurfaceX = np.zeros(Temp.size)
+    for i in range(Temp.size):
+        new_lowersurfaceX[i] = 0.5 * (1 + math.cos(Temp[i]))
+        # print(new_lowersurfaceX[i])
+
     new_lowersurfaceY = lowersurface_interpolate_function(new_lowersurfaceX)
-
-    #Temp = np.append(
-    #    #np.logspace(10, 1, int(uppersurface_panel * 0.4) ,base=0.2),
-    #    np.linspace(0,0.01,10),
-    #    np.linspace(0.010001, 0.9, int(uppersurface_panel * 0.3)),
-    #)
-
-    #new_uppersurfaceX = np.append(
-    #    Temp,
-    #    np.linspace(0.9001, 1, int(uppersurface_panel * 0.3))
-    #).
-
-    Temp = np.logspace(-3 ,1 ,10 ,math.pi/2)
-    Temp2 = np.append(
-        Temp,
-        np.linspace(1.5802 ,math.pi ,uppersurface_panel)
-      )
-    #print(np.linspace(1.5802 ,math.pi ,uppersurface_panel))
-    #print(Temp2)
+    for i in range(new_lowersurfaceX.size):
+        print(new_lowersurfaceX[i], " ", new_lowersurfaceY[i])
+    Temp2 = np.linspace(0, math.pi, int(uppersurface_panel))
     new_uppersurfaceX = np.zeros(Temp2.size)
     for i in range(Temp2.size):
-        new_uppersurfaceX[i] = -(-0.5  + 0.5 * math.cos(Temp2[i]))
-    print("Temp ",Temp2[i])
-    new_uppersurfaceX = np.delete(new_uppersurfaceX ,0)
-    for i in range(Temp2.size):
-        print(new_uppersurfaceX[i])
-
+        new_uppersurfaceX[i] = 0.5 * (1 - math.cos(Temp2[i]))
+    new_uppersurfaceX = np.delete(new_uppersurfaceX, 0)
 
     new_uppersurfaceY = uppersurface_interpolate_function(new_uppersurfaceX)
-
+    for i in range(new_uppersurfaceY.size):
+        print(new_uppersurfaceX[i], " ", new_uppersurfaceY[i])
     new_dataX = np.concatenate([new_lowersurfaceX, new_uppersurfaceX[1:]])
     new_dataY = np.concatenate([new_lowersurfaceY, new_uppersurfaceY[1:]])
     new_data = list(zip(new_dataX, new_dataY))
